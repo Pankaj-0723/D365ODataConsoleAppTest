391caaf994c4e5c340d319eac412e74b3a65c619
Pankaj kumar Sahu <Pankajkumar.S@levtechconsulting.com>
2020-04-09 16:00:13 +0530

    Read me file Updated and data retreiving in the list

--- ODataConsoleApp/SimpleCRUDExamples.cs
+++ ODataConsoleApp/SimpleCRUDExamples.cs
@@ -13,33 +13,33 @@ namespace ODataConsoleApp
             string company = "USMF";
 
             // Create
-           /* d365.AddToAssetMajorTypes(new AssetMajorType
-            {
-                DataAreaId = company,
-                MajorTypeId = assetMajorTypeId,
-                Description = "Description of Test01"
-            });
+            /* d365.AddToAssetMajorTypes(new AssetMajorType
+             {
+                 DataAreaId = company,
+                 MajorTypeId = assetMajorTypeId,
+                 Description = "Description of Test01"
+             });
 
-            // Uncomment below line when you want to save the data.
-            d365.SaveChanges();*/
+             // Uncomment below line when you want to save the data.
+             d365.SaveChanges();*/
 
             // Read
 
             //var assetMajorType = d365.AssetMajorTypes.Where(x => x.DataAreaId == company && x.MajorTypeId == assetMajorTypeId).First();
             // Below line will read Customer data from the service.
-            var customerData = d365.Customers.Where(x => x.DataAreaId == "usmf" && x.CustomerAccount == "DE-001").First();// && x.PartyNumber == "000000838" && x.CustomerGroupId== "90").First();
+            var customerData = d365.Customers.Where(x => x.DataAreaId == "usmf").ToList();// && x.PartyNumber == "000000838" && x.CustomerGroupId== "90").First();
             Console.WriteLine(JsonConvert.SerializeObject(customerData));
             // Below line will read CustomerGroup data from the service.
-            var assetMajorType = d365.CustomerGroups.Where(x => x.DataAreaId == company && x.CustomerGroupId == assetMajorTypeId).First();
+            var assetMajorType = d365.CustomerGroups.Where(x => x.DataAreaId == company).ToList();
             Console.WriteLine(JsonConvert.SerializeObject(assetMajorType));
             //Below line will read data of SalesInvoiceHeader from the service.
-            var salesInvoiceHeaderData = d365.SalesInvoiceHeaders.Where(x => x.DataAreaId == "usmf" && x.InvoiceNumber == "CIV-000001").First();
+            var salesInvoiceHeaderData = d365.SalesInvoiceHeaders.Where(x => x.DataAreaId == "usmf").ToList();// && x.InvoiceNumber == "CIV-000001").First();
             Console.WriteLine(JsonConvert.SerializeObject(salesInvoiceHeaderData));
             // Below line will read data of SalesInvoiceLine from the service
-            var salesInvoiceLineData = d365.SalesInvoiceLines.Where(x => x.DataAreaId == "usmf" && x.InvoiceNumber == "CIV-000001").First();
+            var salesInvoiceLineData = d365.SalesInvoiceLines.Where(x => x.DataAreaId == "usmf").ToList();// && x.InvoiceNumber == "CIV-000001").First();
             Console.WriteLine(JsonConvert.SerializeObject(salesInvoiceLineData));
 
-            Console.WriteLine("Asset Major type of ID {0} successfully created and read.", assetMajorType.Description);
+            //Console.WriteLine("Asset Major type of ID {0} successfully created and read.", assetMajorType.Description);
 
             // Update
             /*assetMajorType.Description = "Updated description";
--- Readme.txt
+++ Readme.txt
@@ -1,1 +1,16 @@
 Copy the OTest.xml file to C:\Temp\ folder.(if no folder like C:\Temp, then create)
+-----------------------------------------------------------------------------------------------------------
+
+Step 1: Add a class library project
+Step 2: Add OData V4 Code Generator in the Library. (Right click on the library soln=> click add new item => Click online=> Search on the right top corner as OData V4=> Select the First one and Add or Install if it is not installed.)
+Step 3: Open the .tt file by double click. Add the OData Uri in the MetadataDocumentUri like below.
+
+public const string MetadataDocumentUri = "https://icerobo-dev-34e4bae11287ed7d1devaos.cloudax.dynamics.com/Data/$metadata";
+
+Step 4: public const string TempFilePath = ""; replace with public const string TempFilePath = "C:\\Temp\\OTest.xml";
+File name you can give anything and path also according to your wish.
+
+Step 5: Save it. It will generate the above in the particular mention path. Build the solution.
+
+Step 6: Open the file in the NotePad++ and press ctrl+shift+f and replace all the "" to " and save it.
+Step 7: Add the rest of the files as per the project and all the references also.
